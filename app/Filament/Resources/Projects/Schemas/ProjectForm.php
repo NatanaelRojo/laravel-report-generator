ce9fa20888edde95f75fd0c2b711a822d1dcfa06
natanael <rojonatanael99@gmail.com>
2026-02-04 14:11:22 -0400

    feat(Projects): create resource structure with Create, Edit, List pages and associated form and table schemas

---
+++ app/Filament/Resources/Projects/Schemas/ProjectForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\Projects\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ProjectForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('user_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+            ]);
+    }
+}

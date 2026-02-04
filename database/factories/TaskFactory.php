49943b11fa6d5c6095da4d56f54a440e49da5c40
natanael <rojonatanael99@gmail.com>
2026-02-04 13:18:34 -0400

    feat(TaskFactory): create factory for Task model with default state and attributes

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\TaskStatus;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'project_id' => Project::factory(),
+            'name' => $this->faker->sentence(4),
+            'description' => $this->faker->paragraph(),
+            'status' => $this->faker->randomElement(TaskStatus::valuesToArray()),
+            'start_date' => $this->faker->optional()->date(),
+            'end_date' => $this->faker->optional()->date(),
+            'due_date' => $this->faker->optional()->date(),
+        ];
+    }
+}

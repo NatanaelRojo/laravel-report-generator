127799488723a4291f8d0267222af7b6a7584c68
natanael <rojonatanael99@gmail.com>
2026-02-04 13:20:03 -0400

    feat(NoteMigration): create migration for notes table with user and task relationships

---
+++ database/migrations/2026_02_04_154958_create_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (! Schema::hasTable('notes')) {
+            Schema::create('notes', function (Blueprint $table) {
+                $table->id();
+                $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+                $table->foreignIdFor(Task::class)->constrained()->onDelete('cascade');
+                $table->text('content');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};

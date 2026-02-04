8d54a10638b687f25d8cdba5a371c2a0643e857e
natanael <rojonatanael99@gmail.com>
2026-02-04 14:23:12 -0400

    feat(Project): add user and tasks relationships to Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Database\Factories\ProjectFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -16,4 +18,14 @@ class Project extends Model
         'name',
         'description',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }

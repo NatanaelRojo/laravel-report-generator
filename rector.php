709cfb7bf3dc597d62337ae7ba5b766ef1c7df58
natanael <rojonatanael99@gmail.com>
2026-02-03 21:52:09 -0400

    Initial commit

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    // register single rule
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ])
+    // here we can define, what prepared sets of rules will be applied
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true
+    )
+    ->withSets([
+                LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+    ]);

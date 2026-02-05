18de2dc9e492b0f1fc3b5230bc56b93fea11c88f
natanael <rojonatanael99@gmail.com>
2026-02-05 11:57:05 -0400

    feat(DateRangeFilter): implement DateRangeFilter class for filtering by date range

---
+++ app/Filament/Filters/DateRangeFilter.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Filters;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Tables\Filters\Filter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+
+class DateRangeFilter
+{
+    public static function make(string $column, ?string $label = null): Filter
+    {
+        $label ??= str($column)->headline();
+
+        return Filter::make($column)
+            ->form([
+                Fieldset::make($label)
+                    ->schema([
+                        DatePicker::make('from')
+                            ->label('From')
+                            ->placeholder('Start Date')
+                            ->native(false), // Optional: makes it look better
+                        DatePicker::make('to')
+                            ->label('To')
+                            ->placeholder('End Date')
+                            ->native(false),
+                    ])
+                    ->columns(1),
+            ])
+            ->query(function (Builder $query, array $data) use ($column) {
+                $query
+                    ->when(
+                        $data['from'],
+                        fn (Builder $query, $date): Builder => $query->whereDate($column, '>=', $date)
+                    )
+                    ->when(
+                        $data['to'],
+                        fn (Builder $query, $date): Builder => $query->whereDate($column, '<=', $date)
+                    );
+            })
+            ->indicateUsing(
+                function (array $data) use ($label): array {
+                    return self::getIndicators($data, $label);
+                }
+                );
+    }
+
+    private static function getIndicators(array $data, string $label): array
+{
+                $indicators = [];
+                
+                if ($data['from'] ?? null) {
+                    $indicators[] = "{$label} from ".Carbon::parse($data['from'])->toFormattedDateString();
+                }
+                if ($data['to'] ?? null) {
+                    $indicators[] = "{$label} to ".Carbon::parse($data['to'])->toFormattedDateString();
+                }
+
+                return $indicators;
+            }
+}

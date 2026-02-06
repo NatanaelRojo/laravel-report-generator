ca7a0f5a0806687abd7aa7529f72da752dbc2743
natanael <rojonatanael99@gmail.com>
2026-02-06 13:30:43 -0400

    feat: add getArrayForFilamentSelector method to HasEnumMethods trait for improved enum handling

--- app/Traits/HasEnumMethods.php
+++ app/Traits/HasEnumMethods.php
@@ -13,4 +13,13 @@ trait HasEnumMethods
     {
         return array_column(self::cases(), 'value');
     }
+
+    public static function getArrayForFilamentSelector(): array
+    {
+        $options = [];
+        foreach (self::cases() as $case) {
+            $options[$case->value] = $case->value;
+        }
+        return $options;
+    }
 }

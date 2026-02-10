855281e4810c8feaf0deff132921d7c23ba78c0a
natanael <rojonatanael99@gmail.com>
2026-02-10 15:33:22 -0400

    feat: enhance TaskFactory to include dynamic due date calculation and verification links

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -29,9 +29,14 @@ class TaskFactory extends Factory
             'title' => $this->faker->sentence(4),
             'description' => $this->faker->paragraph(),
             'status' => $this->faker->randomElement(TaskStatus::valuesToArray()),
+                        'verification_links' => collect(range(1, rand(1, 3)))->map(fn() => [
+                'url' => $this->faker->url(),
+            ])->toArray(),
             'start_date' => $startDate,
             'end_date' => $startDate->copy()->addDays(rand(1, $daysToDueDate)),
-            'due_date' => $startDate->copy()->addDays($daysToDueDate),
+            'due_date' => function (array $attributes) {
+                return fake()->dateTimeBetween($attributes['start_date'], $attributes['end_date']);
+            },
         ];
     }
 }
